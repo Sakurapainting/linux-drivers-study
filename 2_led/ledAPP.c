d26d3be60581730fa637bf0bd900fcf47523b997
Sakurapainting <you2899047197@163.com>
2025-08-16 15:39:24 +0800

    style: 2 app

--- 2_led/ledAPP.c
+++ 2_led/ledAPP.c
@@ -1,27 +1,28 @@
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 /*
-* ./ledAPP <filename> <0:1>      0表示关灯，1表示开灯
-* ./ledAPP /dev/led 0            关灯
-* ./ledAPP /dev/led 1            开灯
-*/
+ * ./ledAPP <filename> <0:1>      0表示关灯，1表示开灯
+ * ./ledAPP /dev/led 0            关灯
+ * ./ledAPP /dev/led 1            开灯
+ */
 
-#define LEDOFF  0       /* 关闭LED */
-#define LEDON   1       /* 打开LED */
+#define LEDOFF 0 /* 关闭LED */
+#define LEDON 1 /* 打开LED */
 
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[])
+{
     int fd = 0;
     int ret = 0;
-    char *filename = NULL;
+    char* filename = NULL;
     unsigned char databuf[1];
 
-    if(argc != 3){
+    if (argc != 3) {
         printf("Usage: %s <filename> <0:1>\n", argv[0]);
         return -1;
     }
@@ -29,7 +30,7 @@ int main(int argc, char *argv[]){
     filename = argv[1]; // 获取设备文件名
 
     fd = open(filename, O_RDWR); // 打开LED设备文件
-    if(fd < 0){
+    if (fd < 0) {
         printf("open %s failed\n", filename);
         return -1;
     }
@@ -37,7 +38,7 @@ int main(int argc, char *argv[]){
     databuf[0] = atoi(argv[2]); // 将命令行参数转换为整数
 
     ret = write(fd, databuf, sizeof(databuf)); // 写入数据到LED设备
-    if(ret < 0){
+    if (ret < 0) {
         printf("led control %s failed\n", filename);
         close(fd);
         return -1;
@@ -46,6 +47,6 @@ int main(int argc, char *argv[]){
     }
 
     close(fd); // 关闭设备文件
-    
+
     return 0;
 }
