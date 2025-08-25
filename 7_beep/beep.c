063bbba945e22022d7109b425d6abee0c9447e9a
Sakurapainting <you2899047197@163.com>
2025-08-25 17:36:18 +0800

    style: 7 driver

--- 7_beep/beep.c
+++ 7_beep/beep.c
@@ -1,29 +1,29 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/fs.h>
-#include <linux/uaccess.h> 
-#include <linux/io.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/gpio.h>
+#include <linux/init.h>
+#include <linux/io.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
+#include <linux/of_gpio.h>
 #include <linux/of_irq.h>
 #include <linux/slab.h>
-#include <linux/of_gpio.h>
-#include <linux/gpio.h>
+#include <linux/uaccess.h>
 
-#define BEEP_CNT     1
-#define BEEP_NAME    "beep"
+#define BEEP_CNT 1
+#define BEEP_NAME "beep"
 
-#define BEEPON  1
+#define BEEPON 1
 #define BEEPOFF 0
 
-struct beep_dev{
-    struct cdev cdev;      // 字符设备结构体
-    struct class *class;   // 设备类
-    struct device *device; // 设备
-    struct device_node *nd; // 设备树节点
+struct beep_dev {
+    struct cdev cdev; // 字符设备结构体
+    struct class* class; // 设备类
+    struct device* device; // 设备
+    struct device_node* nd; // 设备树节点
     dev_t devid;
     int major;
     int minor;
@@ -32,30 +32,33 @@ struct beep_dev{
 
 struct beep_dev beep;
 
-static int beep_open(struct inode *inode, struct file *file) {
+static int beep_open(struct inode* inode, struct file* file)
+{
     file->private_data = &beep; // 将设备结构体指针存储在文件私有数据中
     return 0;
 }
 
-static int beep_release(struct inode *inode, struct file *file) {
+static int beep_release(struct inode* inode, struct file* file)
+{
     // struct beep_dev *dev = (struct beep_dev *)file->private_data;
     return 0;
 }
 
-static ssize_t beep_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos) {
+static ssize_t beep_write(struct file* file, const char __user* buf, size_t count, loff_t* ppos)
+{
     int ret = 0;
     u8 databuf[1];
-    struct beep_dev *dev = (struct beep_dev *)file->private_data;
+    struct beep_dev* dev = (struct beep_dev*)file->private_data;
 
     ret = copy_from_user(databuf, buf, count); // 从用户空间复制数据到内核空间
-    if(ret < 0) {
+    if (ret < 0) {
         return -EFAULT;
     }
 
-    if(databuf[0] == BEEPON) {
+    if (databuf[0] == BEEPON) {
         gpio_set_value(dev->beep_gpio, 0); // 打开蜂鸣器
 
-    } else if(databuf[0] == BEEPOFF) {
+    } else if (databuf[0] == BEEPOFF) {
         gpio_set_value(dev->beep_gpio, 1); // 关闭蜂鸣器
 
     } else {
@@ -68,26 +71,24 @@ static ssize_t beep_write(struct file *file, const char __user *buf, size_t coun
 static const struct file_operations beep_fops = {
     .owner = THIS_MODULE,
     .open = beep_open, // 打开设备的函数
-    .write = beep_write, // 写入设备的函数    
+    .write = beep_write, // 写入设备的函数
     .release = beep_release, // 释放设备的函数
-}; 
+};
 
-
-
-static int __init beep_init(void){
+static int __init beep_init(void)
+{
     int ret = 0;
     // device id
     beep.major = 0;
-    if(beep.major){
+    if (beep.major) {
         beep.devid = MKDEV(beep.major, 0);
         register_chrdev_region(beep.devid, BEEP_CNT, BEEP_NAME);
-    }
-    else{
+    } else {
         alloc_chrdev_region(&beep.devid, 0, BEEP_CNT, BEEP_NAME);
         beep.major = MAJOR(beep.devid);
         beep.minor = MINOR(beep.devid);
     }
-    if(ret < 0) {
+    if (ret < 0) {
         goto fail_devid;
     }
     printk("beep major=%d, minor=%d\n", beep.major, beep.minor);
@@ -96,47 +97,47 @@ static int __init beep_init(void){
     beep.cdev.owner = THIS_MODULE;
     cdev_init(&beep.cdev, &beep_fops);
     ret = cdev_add(&beep.cdev, beep.devid, BEEP_CNT);
-    if(ret < 0){
+    if (ret < 0) {
         goto fail_cdev;
     }
 
     // class
     beep.class = class_create(THIS_MODULE, BEEP_NAME);
-    if(IS_ERR(beep.class)){
+    if (IS_ERR(beep.class)) {
         ret = PTR_ERR(beep.class);
         goto fail_class;
     }
-    
+
     // device
     beep.device = device_create(beep.class, NULL, beep.devid, NULL, BEEP_NAME);
-    if(IS_ERR(beep.device)){
+    if (IS_ERR(beep.device)) {
         ret = PTR_ERR(beep.device);
         goto fail_device;
     }
 
     // node
     beep.nd = of_find_node_by_path("/beep");
-    if(!beep.nd){
+    if (!beep.nd) {
         ret = -EINVAL;
         goto fail_findnd;
     }
 
     // 获取GPIO
     beep.beep_gpio = of_get_named_gpio(beep.nd, "beep-gpios", 0);
-    if(beep.beep_gpio < 0){
+    if (beep.beep_gpio < 0) {
         ret = -EINVAL;
         goto fail_findnd;
     }
 
     // request GPIO
     ret = gpio_request(beep.beep_gpio, "beep-gpio");
-    if(ret < 0){
+    if (ret < 0) {
         goto fail_request;
     }
 
     // 设置GPIO为输出
     ret = gpio_direction_output(beep.beep_gpio, 1);
-    if(ret < 0){
+    if (ret < 0) {
         goto fail_setoutput;
     }
 
@@ -161,7 +162,8 @@ fail_devid:
     return ret;
 }
 
-static void __exit beep_exit(void){
+static void __exit beep_exit(void)
+{
 
     gpio_set_value(beep.beep_gpio, 1); // 确保蜂鸣器关闭
     gpio_free(beep.beep_gpio); // 释放GPIO
@@ -173,8 +175,6 @@ static void __exit beep_exit(void){
     unregister_chrdev_region(beep.devid, BEEP_CNT); // 注销设备号
     printk("beep exit\n");
 }
-
-
 
 module_init(beep_init);
 module_exit(beep_exit);
