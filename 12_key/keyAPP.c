ea982850db1184f4d55c3654c39de2dff88b1d98
Sakurapainting <you2899047197@163.com>
2025-08-26 22:14:40 +0800

    style: 12 APP

--- 12_key/keyAPP.c
+++ 12_key/keyAPP.c
@@ -1,27 +1,28 @@
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 /*
-* ./keyAPP <filename>      
-* ./keyAPP /dev/key   读取按键值                       
-*/
+ * ./keyAPP <filename>
+ * ./keyAPP /dev/key   读取按键值
+ */
 
-#define KEY0VALUE   0xf0    // 自定义
-#define INVALKEY   0x00     // 自定义
+#define KEY0VALUE 0xf0 // 自定义
+#define INVALKEY 0x00 // 自定义
 
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[])
+{
     int val = 0;
     int fd = 0;
     int ret = 0;
-    char *filename = NULL;
+    char* filename = NULL;
     unsigned char databuf[1];
 
-    if(argc != 2){
+    if (argc != 2) {
         printf("Usage: %s <filename>\n", argv[0]);
         return -1;
     }
@@ -29,20 +30,20 @@ int main(int argc, char *argv[]){
     filename = argv[1]; // 获取设备文件名
 
     fd = open(filename, O_RDWR); // 打开KEY设备文件
-    if(fd < 0){
+    if (fd < 0) {
         printf("open %s failed\n", filename);
         return -1;
     }
 
     /* circle read */
-    while(1){
+    while (1) {
         read(fd, &val, sizeof(val)); // 读取按键值
-        if(val == KEY0VALUE){
+        if (val == KEY0VALUE) {
             printf("key0 Press, val = %#x\n", val);
         }
     }
 
     close(fd); // 关闭设备文件
-    
+
     return 0;
 }
