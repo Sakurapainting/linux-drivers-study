70df0182a5be3a4f9b475d88ce0ca1de4b52a404
Sakurapainting <you2899047197@163.com>
2025-08-28 12:44:05 +0800

    style: 14 APP

--- 14_irq/myirqAPP.c
+++ 14_irq/myirqAPP.c
@@ -1,26 +1,27 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 /*
-* ./myirqAPP <filename>      
-* ./myirqAPP /dev/myirqdev                         
-*/
+ * ./myirqAPP <filename>
+ * ./myirqAPP /dev/myirqdev
+ */
 
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[])
+{
     int fd = 0;
     int ret = 0;
-    char *filename = NULL;
+    char* filename = NULL;
     unsigned char data;
     unsigned int cmd = 0;
     unsigned int arg = 0;
 
-    if(argc != 2){
+    if (argc != 2) {
         printf("Usage: %s <filename>\n", argv[0]);
         return -1;
     }
@@ -28,24 +29,24 @@ int main(int argc, char *argv[]){
     filename = argv[1]; // 获取设备文件名
 
     fd = open(filename, O_RDWR); // 打开KEY设备文件
-    if(fd < 0){
+    if (fd < 0) {
         printf("open %s failed\n", filename);
         return -1;
     }
 
     /* circle read */
-    while(1){
+    while (1) {
         ret = read(fd, &data, sizeof(data)); // 读取keyvalue
-        if(ret < 0){
+        if (ret < 0) {
             // 并不是错误处理，而是按键没有生效的情况
         } else {
-            if(data){
+            if (data) {
                 printf("keyvalue = %#x\n", data);
             }
         }
     }
 
     close(fd); // 关闭设备文件
-    
+
     return 0;
 }
