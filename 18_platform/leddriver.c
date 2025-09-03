6554b8be41c090e4d90cc818d2670389048e5f1e
Sakurapainting <you2899047197@163.com>
2025-09-03 16:13:47 +0800

    feat: chapter16 driver 新建

---
+++ 18_platform/leddriver.c
@@ -0,0 +1,42 @@
+#include <asm/io.h>
+#include <asm/mach/map.h>
+#include <asm/uaccess.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/gpio.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/irq.h>
+#include <linux/jiffies.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_gpio.h>
+#include <linux/of_irq.h>
+#include <linux/slab.h>
+#include <linux/spinlock.h>
+#include <linux/timer.h>
+#include <linux/uaccess.h>
+#include <linux/fcntl.h>
+#include <linux/poll.h>
+#include <linux/signal.h>
+#include <linux/platform_device.h>
+
+
+
+static int __init leddriver_init(void) {
+    return 0;
+}
+
+static void  __exit leddriver_exit(void) {
+    
+}
+
+module_init(leddriver_init);
+module_exit(leddriver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("SakoroYou");
