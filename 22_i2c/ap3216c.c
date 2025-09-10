dfe340a40bd5a12332a85437cd6a99b4025f1218
Sakurapainting <you2899047197@163.com>
2025-09-10 18:49:09 +0800

    feat: chapter22 新建

---
+++ 22_i2c/ap3216c.c
@@ -0,0 +1,42 @@
+#include <asm/io.h>
+#include <asm/mach/map.h>
+#include <asm/uaccess.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/gpio.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/irq.h>
+#include <linux/jiffies.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_gpio.h>
+#include <linux/of_irq.h>
+#include <linux/slab.h>
+#include <linux/spinlock.h>
+#include <linux/timer.h>
+#include <linux/uaccess.h>
+#include <linux/input.h>
+#include <linux/i2c.h>
+#include "ap3216creg.h"
+
+static int __init ap3216c_init(void)
+{
+
+    return 0;
+}
+
+static void __exit ap3216c_exit(void)
+{
+
+}
+
+module_init(ap3216c_init);
+module_exit(ap3216c_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("SakoroYou");
