988e329b598008c5ce3cdc8128378c00d41ff781
Sakurapainting <you2899047197@163.com>
2025-09-10 19:05:53 +0800

    feat: chapter22 driver 框架

--- 22_i2c/ap3216c.c
+++ 22_i2c/ap3216c.c
@@ -24,15 +24,50 @@
 #include <linux/i2c.h>
 #include "ap3216creg.h"
 
+static int ap3216c_probe(struct i2c_client* client, const struct i2c_device_id* id){
+    printk("ap3216c_probe!\n");
+    return 0;
+}
+
+static int ap3216c_remove(struct i2c_client* client){
+    return 0;
+}
+
+// 传统匹配表
+static struct i2c_device_id ap3216c_idtable[] = {
+    {"lsc,ap3216c", 0},
+    {}
+};
+
+// 设备树匹配表
+static struct of_device_id ap3216c_of_match[] = {
+    {.compatible = "lsc,ap3216c"},
+    {}
+};
+
+static struct i2c_driver ap3216c_driver = {
+    .probe = ap3216c_probe,
+    .remove = ap3216c_remove,
+    .driver = {
+        .name = "ap3216c",
+        .owner = THIS_MODULE,
+        .of_match_table = of_match_ptr(ap3216c_of_match),
+    },
+    .id_table = ap3216c_idtable,
+};
+
 static int __init ap3216c_init(void)
 {
+    int ret = 0;
+
+    ret = i2c_add_driver(&ap3216c_driver);
 
     return 0;
 }
 
 static void __exit ap3216c_exit(void)
 {
-
+    i2c_del_driver(&ap3216c_driver);
 }
 
 module_init(ap3216c_init);
