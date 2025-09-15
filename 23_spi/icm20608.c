61829bfa366008519ea33a9d9b18c6221a699cf9
Sakurapainting <you2899047197@163.com>
2025-09-15 19:05:48 +0800

    feat: chapter23 driver 片选

--- 23_spi/icm20608.c
+++ 23_spi/icm20608.c
@@ -34,8 +34,11 @@ struct icm20608_dev {
     struct cdev cdev;       // 字符设备
     struct class* class;    // 类
     struct device* device;  // 设备
+    struct device_node* nd; // 设备节点
 
     void* private_data;
+
+    int cs_gpio;            // 片选GPIO
 };
 
 static struct icm20608_dev icm20608dev;
@@ -110,11 +113,49 @@ static int icm20608_probe(struct spi_device* spi){
         goto fail_device;
     }
 
+    // 获取片选引脚
+    icm20608dev.nd = of_get_parent(spi->dev.of_node); // 获取设备节点
+    if (icm20608dev.nd == NULL) {
+        printk("icm20608 get device node failed!\n");
+        ret = -EINVAL;
+        goto fail_nd;
+    }
+
+    icm20608dev.cs_gpio = of_get_named_gpio(icm20608dev.nd, "cs-gpio", 0);
+    if (icm20608dev.cs_gpio < 0) {
+        printk("icm20608 get cs-gpio failed!\n");
+        ret = -EINVAL;
+        goto fail_csgpio;
+    }
+
+    ret = gpio_request(icm20608dev.cs_gpio, "icm20608 cs-gpio");
+    if (ret) {
+        printk("icm20608 cs-gpio request failed!\n");
+        goto fail_request;
+    }
+
+    ret = gpio_direction_output(icm20608dev.cs_gpio, 1); // 输出，高电平
+    if(ret < 0) {
+        printk("icm20608 cs-gpio direction failed!\n");
+        goto fail_setoutput;
+    }
+
+    // 初始化spi_device
+    spi->mode = SPI_MODE_0;
+    spi_setup(spi);
+
     // 设置icm20608dev私有数据
     icm20608dev.private_data = spi;
 
     return 0;
 
+fail_setoutput:
+    gpio_free(icm20608dev.cs_gpio);
+fail_request:
+    of_node_put(icm20608dev.nd);
+fail_csgpio:
+fail_nd:
+    device_destroy(icm20608dev.class, icm20608dev.devid);
 fail_device:
     class_destroy(icm20608dev.class);
 fail_class:
