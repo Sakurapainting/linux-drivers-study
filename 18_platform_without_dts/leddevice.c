e6e8911af68971aff00ee4d2acce2f58ddadb3e1
Sakurapainting <you2899047197@163.com>
2025-09-17 18:22:37 +0800

    style: 18 device

--- 18_platform_without_dts/leddevice.c
+++ 18_platform_without_dts/leddevice.c
@@ -3,6 +3,7 @@
 #include <asm/uaccess.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
+#include <linux/fcntl.h>
 #include <linux/fs.h>
 #include <linux/gpio.h>
 #include <linux/init.h>
@@ -16,52 +17,52 @@
 #include <linux/of_address.h>
 #include <linux/of_gpio.h>
 #include <linux/of_irq.h>
+#include <linux/platform_device.h>
+#include <linux/poll.h>
+#include <linux/signal.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/timer.h>
 #include <linux/uaccess.h>
-#include <linux/fcntl.h>
-#include <linux/poll.h>
-#include <linux/signal.h>
-#include <linux/platform_device.h>
 
 /* 寄存器物理地址 */
-#define CCM_CCGR1_BASE          (0x020C406C) /* 时钟控制_时钟门控 */
-#define SW_MUX_GPIO1_IO03_BASE  (0x020E0068) /* 复用 */
-#define SW_PAD_GPIO1_IO03_BASE  (0x020E02F4) /* 电气属性配置 */
-#define GPIO1_GDIR_BASE         (0x0209C004) /* 方向（输入/输出）寄存器 */
-#define GPIO1_DR_BASE           (0x0209C000) /* 数据寄存器 */
+#define CCM_CCGR1_BASE (0x020C406C) /* 时钟控制_时钟门控 */
+#define SW_MUX_GPIO1_IO03_BASE (0x020E0068) /* 复用 */
+#define SW_PAD_GPIO1_IO03_BASE (0x020E02F4) /* 电气属性配置 */
+#define GPIO1_GDIR_BASE (0x0209C004) /* 方向（输入/输出）寄存器 */
+#define GPIO1_DR_BASE (0x0209C000) /* 数据寄存器 */
 
-#define REGISTER_LEN            4
+#define REGISTER_LEN 4
 
-void leddevice_release(struct device *dev) {
+void leddevice_release(struct device* dev)
+{
     printk("leddevice release\n");
 }
 
 static struct resource led_resources[] = {
     [0] = {
         .start = CCM_CCGR1_BASE,
-        .end   = CCM_CCGR1_BASE + REGISTER_LEN - 1,
+        .end = CCM_CCGR1_BASE + REGISTER_LEN - 1,
         .flags = IORESOURCE_MEM,
     },
     [1] = {
         .start = SW_MUX_GPIO1_IO03_BASE,
-        .end   = SW_MUX_GPIO1_IO03_BASE + REGISTER_LEN - 1,
+        .end = SW_MUX_GPIO1_IO03_BASE + REGISTER_LEN - 1,
         .flags = IORESOURCE_MEM,
     },
     [2] = {
         .start = SW_PAD_GPIO1_IO03_BASE,
-        .end   = SW_PAD_GPIO1_IO03_BASE + REGISTER_LEN - 1,
+        .end = SW_PAD_GPIO1_IO03_BASE + REGISTER_LEN - 1,
         .flags = IORESOURCE_MEM,
     },
     [3] = {
         .start = GPIO1_GDIR_BASE,
-        .end   = GPIO1_GDIR_BASE + REGISTER_LEN - 1,
+        .end = GPIO1_GDIR_BASE + REGISTER_LEN - 1,
         .flags = IORESOURCE_MEM,
     },
     [4] = {
         .start = GPIO1_DR_BASE,
-        .end   = GPIO1_DR_BASE + REGISTER_LEN - 1,
+        .end = GPIO1_DR_BASE + REGISTER_LEN - 1,
         .flags = IORESOURCE_MEM,
     }
 };
@@ -76,11 +77,13 @@ static struct platform_device leddevice = {
     .resource = led_resources,
 };
 
-static int __init leddevice_init(void) {
+static int __init leddevice_init(void)
+{
     return platform_device_register(&leddevice);
 }
 
-static void  __exit leddevice_exit(void) {
+static void __exit leddevice_exit(void)
+{
     platform_device_unregister(&leddevice);
 }
 
